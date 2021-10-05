78ac139db05dd2cd9726144fdaeb04185df6cc64
Rochet2 <tqbattlenet@gmail.com>
2021-10-05 18:08:40 +0300

    Support only current version in migration

--- sql/custom/world/2021_10_05_00_world_objscale.sql
+++ sql/custom/world/2021_10_05_00_world_objscale.sql
@@ -1,9 +1,3 @@
-DELIMITER $$
-DROP PROCEDURE IF EXISTS objscale_update_procedure $$
-CREATE PROCEDURE objscale_update_procedure()
-BEGIN
-
-IF EXISTS(SELECT 1 FROM `updates` WHERE `name` = "2020_09_20_01_world.sql") AND NOT EXISTS(SELECT 1 FROM `updates` WHERE `name` = "2021_03_07_00_world_objscale.sql") AND NOT EXISTS(SELECT 1 FROM `updates` WHERE `name` = "2016_11_10_00_world_objscale.sql") THEN
 
 -- gameobject
 ALTER TABLE `gameobject`ADD COLUMN `size` FLOAT NOT NULL DEFAULT '-1';
@@ -12,10 +6,3 @@ INSERT INTO `command` (`name`, `help`) VALUES ('gobject set scale', 'Syntax: .go
 -- creature
 ALTER TABLE `creature` ADD COLUMN `size` FLOAT NOT NULL DEFAULT '-1';
 INSERT INTO `command` (`name`, `help`) VALUES ('npc set scale', 'Syntax: .npc set scale #scale\r\n\r\nSelected NPC size changed to #scale. NPC scale saved to DB and persistent. Does not affect other creatures of same entry. Using -1 scale uses the default scale from template.');
-
-END IF;
-
-END $$
-CALL objscale_update_procedure() $$
-DROP PROCEDURE IF EXISTS objscale_update_procedure $$
-DELIMITER ;
